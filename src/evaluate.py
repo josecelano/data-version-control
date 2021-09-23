ab185e2ecfc97864ebb4e1df3575a20c65e9fbdd
Jose Celano <josecelano@gmail.com>
2021-09-23 13:39:52 +0100

    Allow to evaluate the model with pre-resized images

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,12 +4,18 @@ from pathlib import Path
 
 from sklearn.metrics import accuracy_score
 
-from train import load_data_from_raw_images
+from train import load_data_from_raw_images, load_data_from_resized_images
 
 
 def main(repo_path):
     test_csv_path = repo_path / "data/prepared/test.csv"
+
+    # evaluate using raw images
     test_data, labels = load_data_from_raw_images(test_csv_path)
+
+    # evaluate using pre-resized images
+    #test_data, labels = load_data_from_resized_images(test_csv_path)
+
     model = load(repo_path / "model/model.joblib")
     predictions = model.predict(test_data)
     accuracy = accuracy_score(labels, predictions)
